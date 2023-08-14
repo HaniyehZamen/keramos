9c26055ee5819f63ebfce1a1c65fc76e161316d2
HaniyehZamen <hz1379lmh@gmail.com>
2023-08-14 15:50:08 +0330

    1402.5.23

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about', views.about, name='about'),
-    path('contact',views.contact_us,name='contact_us')
+    # path('contact',views.contact_us,name='contact_us')
+    path('contact',views.Contact_us.as_view(),name='contact_us')
 ]
 
--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 
 def home(request):
@@ -8,5 +9,8 @@ def home(request):
 def about(request):
     return render(request, 'about.html', {})
 
-def contact_us(request):
-    return render(request,'contact_us.html',{})
+# def contact_us(request):
+#     return render(request,'contact_us.html',{})
+
+class Contact_us (TemplateView):
+    template_name = 'contact_us.html'
